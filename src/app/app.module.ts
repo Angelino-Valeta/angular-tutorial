46c3710022353296a3b4f92caa310e5529e04672
Angelino-Valeta <angelinovaleta@gmail.com>
2021-09-29 23:10:20 +0100

    import modules and Declaration components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent],
+  imports: [BrowserModule, RouterModule, ReactiveFormsModule],
+  declarations: [AppComponent, TopBarComponent, ProductListComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
